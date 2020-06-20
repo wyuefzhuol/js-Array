08f1067cb1a03a0ef187d2924e01ffef63682df7
Eric <1527169088@qq.com>
2020-06-20 15:12:39 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+// The writer determines whether the following variables are of type array.
+var a = '[a, b, c, d]';
+var b = [1, 2, 3, 4];
+//TODO
+console.log(a instanceof Array);
+console.log(b instanceof Array);
+
+// Write a program that multiplies each entry in the following array by 2。
+var a = [1, 2, 3, 4, 5];
+// TODO should output [2,4,6,8,10]
+a.forEach((item, index) => {     
+    a[index]=item*2;
+})
+console.log(a)
+
+// Write the program, according to the following requirements output results.
+var colors = ["Red", "Green", "White", "Black"];
+//TODO case 1 output: 'Red Green White Black'
+// case 2 output: 'Red+Green+White+Black'
+// case 3 output: 'Red,Green,White,Black'
+console.log(colors.join(" "));
+console.log(colors.join("+"));
+console.log(colors.join(","));
+
+
+// Write a program to sort the Numbers in the following array from largest to smallest.
+var a = [5, 1, 8, 10, 4];
+//TODO should output: [10,8,5,4,1]
+console.log(a.sort(function(a, b){return b - a}));
+
+// Program to find the most frequent element in the following array.
+var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+//TODO should output: 'a'
+let res = {};
+let maxName, maxNum = 0;
+a.forEach((item) => {
+    res[item] ? res[item] += 1 : res[item] = 1;
+})
+for (let r in res) {
+    if (res[r] > maxNum) {
+        maxNum = res[r];
+        maxName = r;
+    }
+}
+console.log(maxName);
